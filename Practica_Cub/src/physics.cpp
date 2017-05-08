8e85cbe06d2e40ebf73438d75672b4e66f18cfbd
Anna Ponce Llerda <User@USER-PC>
2017-05-08 22:41:42 +0200

    ghvfhyjdfcfdcj

--- Practica_Cub/src/physics.cpp
+++ Practica_Cub/src/physics.cpp
@@ -16,14 +16,20 @@ float y = 0;
 float z = 0;
 float rotZ = 0;
 float random = 0;
+int dis;
 glm::vec3 randomize;
+glm::vec3 normal;
+glm::vec3 temp;
+glm::vec3 derP;
+float cont = 0.f;
 
 struct Cub {
+	float w, d, h = 2.f;
 	float massa = 1;
 	float gravetat = 9.8f;
 	float forsa = 5.f;
 	glm::vec3 f = glm::vec3(0, forsa, 0);
-	glm::vec3 centreMassa = glm::vec3(0.f, 0.f, 0.f);
+	glm::vec3 centreMassa = glm::vec3(0.f, 8.f, 0.f);
 	glm::vec3 force = glm::vec3(0, -gravetat, 0);
 	glm::vec3 x;
 	glm::vec3 v;
@@ -34,9 +40,32 @@ struct Cub {
 	glm::vec3 l;
 	glm::vec3 tau;
 	glm::vec3 angularVel;
+	glm::vec3 A = glm::vec3(-w / 2, h / 2, -d / 2);
+	glm::vec3 B = glm::vec3(w / 2, h / 2, -d / 2);
+	glm::vec3 C = glm::vec3(w / 2, -h / 2, -d / 2);
+	glm::vec3 D = glm::vec3(-w / 2, -h / 2, -d / 2);
+	glm::vec3 E = glm::vec3(w / 2, -h / 2, d / 2);
+	glm::vec3 F = glm::vec3(w / 2, h / 2, d / 2);
+	glm::vec3 G = glm::vec3(-w / 2, h / 2, d / 2);
+	glm::vec3 H = glm::vec3(-w / 2, -h / 2, d / 2);
+
 	bool primer;
 	glm::quat quat = glm::quat(0, 0, 0, 0);
 
+	
+};
+
+Cub *cub = new Cub();
+
+glm::vec3 vertexs[] = {
+	glm::vec3(-cub->w / 2.f, cub->h / 2.f, -cub->d / 2.f),
+	glm::vec3(cub->w / 2.f, cub->h / 2.f, -cub->d / 2.f),
+	glm::vec3(cub->w / 2.f, -cub->h / 2.f, -cub->d / 2.f),
+	glm::vec3(-cub->w / 2.f, -cub->h / 2.f, -cub->d / 2.f),
+	glm::vec3(-cub->w / 2.f, cub->h / 2.f, cub->d / 2.f),
+	glm::vec3(cub->w / 2.f, cub->h / 2.f, cub->d / 2.f),
+	glm::vec3(cub->w / 2.f, -cub->h / 2.f, cub->d / 2.f),
+	glm::vec3(-cub->w / 2.f, -cub->h / 2.f, cub->d / 2.f),
 };
 
 namespace Cubo {
@@ -61,7 +90,8 @@ void GUI() {
 	}
 }
 
-Cub *cub = new Cub();
+
+
 
 void PhysicsInit() {
 
@@ -99,9 +129,78 @@ void PhysicsUpdate(float dt) {
 	glm::mat4 matrix;
 	matrix = glm::translate(matrix, cub->x);
 	matrix = matrix*glm::mat4_cast(cub->quat); //apliquem la rotacio a la matrix passant altra vegada el quat a mat4
-	Cubo::updateCubo(matrix);
 	
+	
+	//------------COLISIONS--------------------------------------
+	// Floor Colision
+	normal = glm::vec3(0, 1, 0);
+	dis = 0;
+	derP = cub->v + glm::cross(cub->angularVel, (cub->D - cub->x));
+
+	/*if ((glm::dot(normal, derP) + dis)*((glm::dot(normal, cub->D) + dis)) < 0) {
+	pC[i].pos = pC[i].pos - (1 + coefElasticity) * (glm::dot(normal, pC[i].pos) + dis)*normal;
+	pC[i].vel = pC[i].vel - (1 + coefElasticity) * (glm::dot(normal, pC[i].vel))* normal - coefFriction*(pC[i].vel - temp);
+	}
+
+	// Top Colision
+	temp = glm::dot(normal, pC[i].vel) * normal;
+	normal = glm::vec3(0, -1, 0);
+	dis = 10;
+	if ((glm::dot(normal, pC[i].pos) + dis)*((glm::dot(normal, initial) + dis)) < 0) {
+	pC[i].pos = pC[i].pos - (1 + coefElasticity) * (glm::dot(normal, pC[i].pos) + dis)*normal;
+	pC[i].vel = pC[i].vel - (1 + coefElasticity) * (glm::dot(normal, pC[i].vel))* normal - coefFriction*(pC[i].vel - temp);
+	}
+
+	// Right Face Colision
+	normal = glm::vec3(-1, 0, 0);
+	temp = glm::dot(normal, pC[i].vel) * normal;
+	dis = 5;
+	if ((glm::dot(normal, pC[i].pos) + dis)*((glm::dot(normal, initial) + dis)) < 0) {
+	pC[i].pos = pC[i].pos - (1 + coefElasticity) * (glm::dot(normal, pC[i].pos) + dis)*normal;
+	pC[i].vel = pC[i].vel - (1 + coefElasticity) * (glm::dot(normal, pC[i].vel))* normal - coefFriction*(pC[i].vel - temp);
+	}
+
+	// Left Face Colision
+	normal = glm::vec3(1, 0, 0);
+	temp = glm::dot(normal, pC[i].vel) * normal;
+	dis = 5;
+	if ((glm::dot(normal, pC[i].pos) + dis)*((glm::dot(normal, initial) + dis)) < 0) {
+	pC[i].pos = pC[i].pos - (1 + coefElasticity) * (glm::dot(normal, pC[i].pos) + dis)*normal;
+	pC[i].vel = pC[i].vel - (1 + coefElasticity) * (glm::dot(normal, pC[i].vel))* normal - coefFriction*(pC[i].vel - temp);
+
+	}*/
+
+	// Front Face Colision
+	normal = glm::vec3(0, 0, -1);
+	derP = cub->v + glm::cross(cub->angularVel, (cub->D - cub->x));
+	float velr = glm::dot(normal, derP);
+	glm::vec3 posT = cub->D*velr*dt;
+	dis = 5;
+	//	if ((glm::dot(normal, derP) + dis)*((glm::dot(normal, cub->D) + dis)) < 0) {
+	//if ((glm::dot(normal, posT) + dis)*((glm::dot(normal, cub->D) + dis)) < 0) {
+	if ((velr + dis)*(glm::dot(normal, cub->D) + dis) < 0) { //nidea tu
+		std::cout << "He colis amb D front";
+	}
+
+	// Back Face Colision
+	/*normal = glm::vec3(0, 0, 1);
+	temp = glm::dot(normal, pC[i].vel) * normal;
+	dis = 5;
+	//int deb = -(particlesContainer[i].pos.x*normal.x) - (particlesContainer[i].pos.y*normal.y) - (particlesContainer[i].pos.z*normal.z);
+	//std::cout << deb;
+	if ((glm::dot(normal, pC[i].pos) + dis)*((glm::dot(normal, initial) + dis)) < 0) {
+	pC[i].pos = pC[i].pos - (1 + coefElasticity) * (glm::dot(normal, pC[i].pos) + dis)*normal;
+	pC[i].vel = pC[i].vel - (1 + coefElasticity) * (glm::dot(normal, pC[i].vel))* normal - coefFriction*(pC[i].vel - temp);
+
+	}*/
+
+	//collisions
+	/*for (int i = 0; i < 8; i++) {
+
+	}*/
+	Cubo::updateCubo(matrix);
 }
 void PhysicsCleanup() {
 	//TODO
+	Cubo::cleanupCubo();
 }
